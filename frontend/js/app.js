9c82b7831df80c01fbbef17e9e90541469054be7
EduChezz <javieresanchezy@gmail.com>
2026-06-09 01:42:33 -0500

    🎮 Fix minijuegos + cerrar modal + consecuencias + logros persistidos

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -779,7 +779,7 @@ function actualizarFondoBosque(salud) {
 function actualizarImagenCriatura(imagenActual, fase, tipo) {
     dom.criaturaSprite.innerHTML = ''
     const img = document.createElement('img')
-    img.src   = imagenActual || '/assets/images/criatura/huevo.png'
+    img.src   = imagenActual || '/assets/images/criatura/sylvae_base_paz.gif'
     img.alt   = 'Sylvae'
     img.onerror = () => { dom.criaturaSprite.innerHTML = fase === 'huevo' ? '🥚' : '🐾'; dom.criaturaSprite.style.fontSize = '7rem' }
     dom.criaturaSprite.appendChild(img)
